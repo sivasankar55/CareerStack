db2929b1064ff5932b132d950f3b5df4ba4415a7
sivasankar55 <sivasankar8875@gmail.com>
2025-08-01 13:09:07 +0530

    added limit to image size

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const Port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
