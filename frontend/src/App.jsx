5a2288f1d5b62b91553cd2b5315a92606706708e
sivasankar55 <sivasankar8875@gmail.com>
2025-08-07 12:16:17 +0530

    added notification ui and funciton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,67 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import { Navigate, Route, Routes } from 'react-router-dom'
-
-import Layout from './components/layout/Layout'
-import SignUpPage from './pages/auth/SignUpPage';
-import LoginPage from './pages/auth/LoginPage';
-import HomePage from './pages/HomePage';
-import toast, { Toaster } from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query';
-import { axiosInstance } from './lib/axios';
+import Layout from "./components/layout/Layout";
+import SignUpPage from "./pages/auth/SignUpPage";
+import LoginPage from "./pages/auth/LoginPage";
+import HomePage from "./pages/HomePage";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+import NotificationsPage from "./pages/NotificationsPage";
 
 function App() {
-
-  const {data:authUser, isLoading} = useQuery({
+  const { data: authUser, isLoading } = useQuery({
     queryKey: ["authUser"],
-    queryFn:async () => {
+    queryFn: async () => {
       try {
         const res = await axiosInstance.get("/auth/me");
         return res.data;
       } catch (error) {
-        if(error.response && error.response.status === 401)
-          return null
+        if (error.response && error.response.status === 401) return null;
       }
       toast.error(error.response.data.message || "Something went wrong");
-    }
+    },
   });
 
-  if(isLoading) return null
- 
+  if (isLoading) return null;
+
   return (
-  <Layout>
-  <Routes>
-    <Route path="/" element={authUser ? <HomePage/> : <Navigate to={"/login"}/>}/>
-    <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/>}/>
-    <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}/>
-  </Routes>
-  <Toaster/>
-  </Layout>
-  )
+    <Layout>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            authUser ? <NotificationsPage /> : <Navigate to={"/login"} />
+          }
+        />
+        {/* <Route
+          path="/network"
+          element={authUser ? <NetworkPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/post/:postId"
+          element={authUser ? <PostPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
+        /> */}
+      </Routes>
+      <Toaster />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/NetworkPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NetworkPage = () => {
+  return (
+    <div>NetworkPage</div>
+  )
+}
+
+export default NetworkPage
